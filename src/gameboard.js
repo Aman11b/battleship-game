0b93cbabf8d1f2a1c4d6aba39b7b204909a0bec2
Aman11b <amansingh1100b@gmail.com>
2024-12-14 21:01:28 +0530

    added gameboard tests and its module

---
+++ src/gameboard.js
@@ -0,0 +1,79 @@
+const GameBoard = () => {
+    const ships = []; //All ships
+    const boardSize=10;
+    const missedAttacks=[];
+    const attackedCoordinates=new Set();
+
+    const placeShip = (ship, startCoord, orientation) => {
+        const [x,y]=startCoord;
+        const coordinates = [];
+
+        if(
+            (orientation === 'horizontal' && y+ship.length >boardSize) ||
+            (orientation === 'vertical' && x+ship.length >boardSize)
+        ){
+            throw new Error('Ship placement is out of bound');
+        }
+
+        for(let i = 0; i<ship.length; i++){
+            if(orientation === 'horizontal') {
+                coordinates.push([x,y+i]);
+            }else if (orientation === 'vertical') {
+                coordinates.push ([x+i,y]);
+            }
+        }
+
+        for(const placeShip of ships){
+            for(const placeCoord of placeShip.coordinates){
+                for(const coord of coordinates){
+                    if(
+                        placeCoord[0] === coord[0] &&
+                        placeCoord[1] === coord[1]
+                    ){
+                        throw new Error ('Ships cannot overlap');
+                    }
+                }
+            }
+        }
+
+        ship.coordinates=coordinates;
+        ships.push(ship);
+    };
+
+    const receiveAttack=(coordinate)=>{
+        const coordString=coordinate.toString();
+
+        if(attackedCoordinates.has(coordString)){
+            throw new Error("Coordinate already hit");
+        }
+        attackedCoordinates.add(coordString);
+
+        for(const ship of ships){
+            for(const coord of ship.coordinates){
+                if(
+                    coord[0] === coordinate[0] &&
+                    coord[1] === coordinate[1]
+                ){
+                    ship.hit();
+                    return;
+                }
+            }
+        }
+
+        missedAttacks.push(coordinate);
+    };
+
+    const allShipSunk=()=>{
+        return ships.every((ship)=>ship.isSunk());
+    }
+
+    return{
+        ships,
+        missedAttacks,
+        allShipSunk,
+        placeShip,
+        receiveAttack,
+    };
+};
+
+export default GameBoard;
---
+++ test/gameboard.test.js
@@ -0,0 +1,101 @@
+import { experiments } from "webpack";
+import GameBoard from "../src/gameboard";
+import Ship from '../src/ship';
+
+describe('GameBoard Factory',()=>{
+    test('placeShip() places a ship at specific position',()=>{
+        const gameBoard=GameBoard();
+        const ship=Ship(3);
+
+        gameBoard.placeShip(ship,[0,0],'horizontal');
+
+        expect(gameBoard.ships[0].coordinates).toEqual([
+            [0,0],
+            [0,1],
+            [0,2]
+        ]);
+    });
+
+    test('placeShip() prevents ships from being placed out of bound',() =>{
+        const gameBoard=GameBoard();
+        const ship=Ship(3);
+
+        expect(()=> gameBoard.placeShip(ship,[0,9],'horizontal')).toThrow('Ship placement is out of bound');
+    });
+
+    test('placeShip() prevents overlapping ship placement',()=>{
+        const gameBoard=GameBoard();
+        const ship1=Ship(3);
+        const ship2=Ship(4);
+
+        gameBoard.placeShip(ship1,[2,2],'horizontal');
+
+        expect(()=>gameBoard.placeShip(ship2,[2,3],'horizontal')).toThrow(
+            'Ships cannot overlap'
+        );
+    });
+
+    test('receivedAttack() register a hit on a ship',()=>{
+        const gameBoard=GameBoard();
+        const ship=Ship(3);
+
+        gameBoard.placeShip(ship,[2,2],'horizontal');
+
+        gameBoard.receiveAttack([2,3]);
+
+        expect(ship.hits()).toBe(1);
+    });
+
+    test('receiveAttack() register a missed attack',()=>{
+        const gameBoard=GameBoard();
+        const ship=Ship(3);
+
+        gameBoard.placeShip(ship,[2,2],'horizontal');
+
+        gameBoard.receiveAttack([5,5]);
+
+        expect(gameBoard.missedAttacks).toContainEqual([5,5]);
+    });
+
+    test('receiveAttack() prevent attack the same coordinate twice ',()=>{
+        const gameBoard=GameBoard();
+        const ship=Ship(3);
+
+        gameBoard.placeShip(ship,[2,2],'horizontal');
+
+        gameBoard.receiveAttack([2,3]);
+        expect(()=> gameBoard.receiveAttack([2,3])).toThrow('Coordinate already hit');
+    });
+
+    test('allShipSink() return false if not all ships are sunk',()=>{
+        const gameBoard=GameBoard();
+        const ship1=Ship(2);
+        const ship2=Ship(3);
+
+        gameBoard.placeShip(ship1,[0,0],'horizontal');
+        gameBoard.placeShip(ship2,[2,2],'vertical');
+
+        gameBoard.receiveAttack([0,0]);
+        gameBoard.receiveAttack([0,1]);
+
+        expect(gameBoard.allShipSunk()).toBe(false);
+    });
+
+    test('allShipSunk() return true if all ships are sunk',()=>{
+        const gameBoard=GameBoard();
+        const ship1=Ship(2);
+        const ship2=Ship(3);
+
+        gameBoard.placeShip(ship1,[0,0],'horizontal');
+        gameBoard.placeShip(ship2,[2,2],'vertical');
+
+        gameBoard.receiveAttack([0, 0]);
+        gameBoard.receiveAttack([0, 1]);
+        gameBoard.receiveAttack([2, 2]);
+        gameBoard.receiveAttack([3, 2]);
+        gameBoard.receiveAttack([4, 2]);
+
+        expect(gameBoard.allShipSunk()).toBe(true);
+    });
+
+});
